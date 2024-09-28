456f389838fd25568d35c40e49e8a75e39932e23
dave17code <dave17code@gmail.com>
2024-09-28 15:08:23 +0900

    chapter11

---
+++ chapter12.js
@@ -0,0 +1,13 @@
+// function add(a, b, callback) {
+//     setTimeout(() => {
+//         const sum = a + b;
+//         callback(sum);
+//     }, 3000);
+// }
+
+// add(1, 2, (value) => {
+//     console.log(value);
+// });
+
+// 음식을 주문하는 상황
+
